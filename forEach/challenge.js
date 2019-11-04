d02960a0c359371fe527db75fb08a119b7b96725
Accio26 <acc1iela@gmail.com>
2019-11-05 01:23:10 +0900

    forEach コーディングチャレンジまで終了

---
+++ forEach/challenge.js
@@ -0,0 +1,21 @@
+// forループからの脱却
+
+function handlePosts() {
+  var posts = [{
+      id: 23,
+      title: 'JSニュース'
+    },
+    {
+      id: 52,
+      title: 'リファクター・シティ'
+    },
+    {
+      id: 105,
+      title: 'Rubyのいいところ'
+    },
+  ];
+
+  posts.forEach(function (post) {
+    savePost(posts[i]);
+  });
+}
