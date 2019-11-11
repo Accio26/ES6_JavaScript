f9ff9e8a987b013ff4c78ae4c716117cc944bfbc
Accio26 <acc1iela@gmail.com>
2019-11-11 23:24:12 +0900

    template_r 終了

---
+++ template_r/challenge.js
@@ -0,0 +1,13 @@
+// テンプレートリテラルを使ってみよう
+// 次の関数で、テンプレートリテラルを使うようにリファクタしてください
+function doubleMessage(number) {
+  return `${number}を2倍すると${2 * number}になります`;
+}
+
+
+// 氏名ヘルパー関数を作ってみよう
+// 次の関数を点レプートリテラルを使ってリファクタしてください
+
+function fullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
---
+++ template_r/index.js
@@ -0,0 +1,8 @@
+function getMessage() {
+  // const year = Date().getFullYear();
+
+  return `今年は${new Date().getFullYear()}年です。`;
+  // JavaScriptだったらなんでもかける:)
+}
+
+getMessage();
